d73b51689d5969182e808d964a40535f6ee7e8c4
Qing Yang <qyang.kevin@gmail.com>
2019-12-09 18:00:24 -0700

    code complete, needs testing and documentation

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
+import sys
 import argparse as ap
 import pandas as pd
 import numpy as np
 import subprocess
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
+import plot
+import motif_enrichment as enrichment
 from sklearn.preprocessing import StandardScaler
-import seaborn as sns; sns.set()
 
 
 def parse_args():
@@ -30,11 +30,25 @@ def parse_args():
                         default='./data/raw_counts.txt',
                         required=False)
 
+    parser.add_argument('-k',
+                        '--kmeans',
+                        type=str,
+                        help="Manually select a k for clustering",
+                        default=None,
+                        required=False)
+
     parser.add_argument('-n',
                         '--num_genes',
                         type=str,
-                        help="Number of genes to plot",
+                        help="Number of genes to plot initial heatmap etc.",
                         default=500,
+                        required=False)
+
+    parser.add_argument('-a',
+                        '--ame',
+                        type=str2bool,
+                        help="Enable AME after installing AME",
+                        default=False,
                         required=False)
 
     return parser.parse_args()
@@ -51,8 +65,8 @@ def read_counts(countfile):
             counts_header = l.rstrip().split("\t")
         else:
             gene_count = l.rstrip().split("\t")
+            gene_count[2] = gene_count[2].split(', ', 1)[0]
             # Remove version number from refseq accession number
-            # gene_count[0] = gene_count[0].split('.', 1)[0]
             counts.append(gene_count)
     return counts_header, counts
 
@@ -64,7 +78,7 @@ def sort_counts(in_counts):
     # Turning off pandas SettingWithCopyWarning
     pd.set_option('mode.chained_assignment', None)
     # Some array's SNR is too low, filter and resort
-    order = ['SYMBOL', '0.15H.IFN_1', '0.5H.IFN_1', '0.75H.IFN_1',
+    order = ['ACCNUM', '0.15H.IFN_1', '0.5H.IFN_1', '0.75H.IFN_1',
        '1H.IFN_1', '1.25H.IFN_1', '1.5H.IFN_1', '2.25H.IFN_1', '2.75H.IFN_1',
        '3H.IFN_1', '3.5H.IFN_1', '5H.IFN_1', '5.5H.IFN_1', '6H.IFN_1',
        '6.5H.IFN_1', '7H.IFN_1', '8H.IFN_1', '9H.IFN_1', '10H.IFN_1',
@@ -79,80 +93,8 @@ def sort_counts(in_counts):
     x_var = np.var(x_sub, 1, dtype=np.float64)
     in_counts['Variance'] = x_var
     in_counts.sort_values(by=['Variance'], inplace=True, ascending=False)
-    return in_counts.iloc[:,:-2]
+    return in_counts.iloc[0:2000,:-2]
 
-
-def plot_pca(out, counts):
-    '''
-    Function to plot PCA for microarray datasets
-    '''
-    counts = counts.iloc[:,1:]
-    x = np.transpose(counts.values)
-    x = StandardScaler().fit_transform(x)
-    pca = PCA(n_components=2)
-    principalComponents = pca.fit_transform(x)
-    principalDf = pd.DataFrame(data = principalComponents
-                 , columns = ['PC1', 'PC2'])
-    finalDf = pd.concat([principalDf, pd.DataFrame(counts.columns)], axis = 1)
-    fig = plt.figure(figsize = (8,8))
-    ax = fig.add_subplot(1,1,1)
-    ax.set_xlabel('PC1', fontsize = 15)
-    ax.set_ylabel('PC2', fontsize = 15)
-    ax.set_title('2 Component PCA', fontsize = 20)
-    ax.scatter(finalDf.loc[:, 'PC1']
-               , finalDf.loc[:, 'PC2']
-               , s = 50)
-    for i, txt in enumerate(counts.columns):
-        ax.annotate(txt, (finalDf.loc[i, 'PC1'], finalDf.loc[i, 'PC2']))
-
-    ax.grid()
-    plt.savefig(out+'/sample_pca.png', bbox_inches='tight')
-    plt.close()
-
-
-def plot_heatmap(out, counts, num_genes):
-    '''
-    Plot a heatmap of gene expression over time for top num_genes genes with
-    the greatest variance
-    '''
-    select_counts = counts.iloc[0:num_genes,1:].astype(float)
-    select_counts = select_counts.sub(select_counts.iloc[:,0], axis='rows')
-    gene_names = counts.iloc[0:num_genes,1]
-    # Normalize it by row:
-    # (not sure if it is the best way, please feel free to give me a better method.)
-    # 1: substract mean
-    df_norm_row=select_counts.sub(select_counts.mean(axis=1), axis=0)
-    # 2: divide by standard dev
-    df_norm_row=df_norm_row.div(select_counts.std(axis=1), axis=0 )
-
-    xlabel = [x.replace('.IFN_1','') for x in select_counts.columns]
-    xlabel = [x.replace('H',' hr') for x in xlabel]
-    if num_genes > 50:
-        ax = sns.clustermap(df_norm_row,
-                         metric="correlation", col_cluster=False,
-                         xticklabels=xlabel,
-                         cmap="RdBu_r")
-    else:
-        ax = sns.clustermap(df_norm_row,
-                         metric="correlation", col_cluster=False,
-                         xticklabels=xlabel,
-                         yticklabels=gene_names,
-                         cmap="RdBu_r")
-    plt.savefig(out+'/gene_heatmap.png', bbox_inches='tight')
-    plt.close()
-
-def plot_trajectory(out, counts, num_genes):
-    select_counts = counts.iloc[0:num_genes,1:].astype(float)
-    xlabel = [x.replace('.IFN_1','') for x in select_counts.columns]
-    xlabel = [float(x.replace('H','')) for x in xlabel]
-    select_counts = select_counts.sub(select_counts.iloc[:,0], axis='rows')
-    fig = plt.figure(figsize = (8,8))
-    ax = fig.add_subplot(1,1,1)
-    ax.set_ylabel('Gene Expression Fold Change', fontsize = 15)
-    ax.set_xlabel('Time (hrs)', fontsize = 15)
-    ax.plot(xlabel, select_counts.T)
-    plt.savefig(out+'/gene_trajectory.png', bbox_inches='tight')
-    plt.close()
 
 def main():
     args = parse_args()
@@ -162,17 +104,46 @@ def main():
 
     num_genes = int(args.num_genes)-1
     out_dir = args.out_dir
+
     # plot heatmap
     print('Generating Gene Expression Heatmap ...')
-    plot_heatmap(out_dir, counts, num_genes)
+    plot.plot_heatmap(out_dir, counts, num_genes)
+
     # Plot gene expression tragectory
     print('Generating Gene Expression Trajectory ...')
-    plot_trajectory(out_dir, counts, num_genes)
+    plot.plot_trajectory(out_dir, counts, num_genes)
+
+    # Save formatted and sorted count file
     counts.to_csv('data/counts_clust.txt', index = False, sep='\t')
 
+    # Call clust to cluster the genes
     print('Clustering Genes (this might take awhile) ...')
     subprocess.call(['mkdir', 'clust_out'])
-    subprocess.call(['clust', 'data/counts_clust.txt', '-o', './clust_out'])
+    if args.kmeans is not None:
+        subprocess.call(['clust', 'data/counts_clust.txt',
+                         '-o', './clust_out', '-K', args.kmeans])
+    else:
+        subprocess.call(['clust', 'data/counts_clust.txt', '-o', './clust_out'])
+
+    # Make sure everythin is all set for motif enrichment
+    clust_out = './clust_out/Clusters_Objects.tsv'
+    ref_bed = './ref/mm10.refseq.bed'
+    ref_fa = './ref/mm10.fa'
+    ref_motif = './ref/HOCOMOCOv11_core_MOUSE_mono_meme_format.meme'
+
+    try:
+        open(clust_out)
+    except FileNotFoundError:
+        print('Clustering failed, '
+               +'Clusters_Objects.tsv not found in the output directory')
+
+
+    print('Motif Enrichment Analysis (this might take awhile) ...')
+    enrichment.run_motif_enrichment(clust_out,
+                                    ref_bed,
+                                    ref_fa, ref_motif,
+                                    args.ame,
+                                    100, 100)
 
 
 if __name__ == '__main__':
---
+++ motif_enrichment.py
@@ -0,0 +1,94 @@
+import argparse
+import sys
+import os
+import pandas as pd
+import pybedtools
+import subprocess
+
+def gene_fetcher(gene_list, promoter_output, bed_df, upstream, downstream, fasta_file):
+    # takes an input list and fetches the genomic location, returning the promoter
+    # of each gene
+
+    # BED6 format
+    gene_name_index = 3
+    chr_index = 0
+    start_index = 1
+    stop_index = 2
+    strand_index = 5
+
+    genes = gene_list
+    output = open(promoter_output, 'w')
+    i = 0
+    for line in genes:
+        try:
+            g = line.rstrip()
+        except AttributeError:
+            print(line)
+        # First line only has one common entry called 'Genes'
+        if i == 0:
+            i += 1
+            continue
+
+        # Start from the second line idx=1
+        try:
+            gene_idx = bed_df.loc[bed_df['acc']==g].index[0]
+        except IndexError:
+            print(g + ' not found')
+            gene_idx = -1
+            continue
+
+        if gene_idx != -1:
+            # if the gene matches, write the promoter to a file
+            if bed_df.iloc[gene_idx, strand_index] == '-':
+                prom_start = int(bed_df.iloc[gene_idx, stop_index]) + upstream
+                prom_end = int(bed_df.iloc[gene_idx, stop_index]) - downstream
+                if prom_end < 1:
+                    prom_end = 1
+                string =  bed_df.iloc[gene_idx, chr_index] + '\t' + str(prom_end) +\
+                          '\t' + str(prom_start) + '\t' + g + '\t' + '1' + '\t' + '-' + '\n'
+                a = pybedtools.BedTool(string, from_string=True)
+                a = a.sequence(fi = fasta_file)
+                final = open(a.seqfn).read()
+                output.write(final)
+            if bed_df.iloc[gene_idx, strand_index] == '+':
+                prom_start = int(bed_df.iloc[gene_idx, start_index]) - upstream
+                if prom_start < 1:
+                    prom_start = 1
+                prom_end = int(bed_df.iloc[gene_idx, start_index]) + downstream
+                string = bed_df.iloc[gene_idx, chr_index] +'\t' + str(prom_start) + '\t' + str(prom_end) +\
+                         '\t' + g + '\t' + '1' + '\t' + '+' + '\n'
+                a = pybedtools.BedTool(string, from_string=True)
+                a = a.sequence(fi = fasta_file)
+                final = open(a.seqfn).read()
+                output.write(final)
+        else:
+            print('Gene: '+g+' not found.')
+            continue
+    output.close()
+    # sys.exit(0)
+
+def run_motif_enrichment(clust_out, ref_bed, ref_fa, ref_motif, ame, up_dist = 100, down_dist = 100):
+    '''
+    Wraper function that parse out genes and extract promoter sequence from reference files
+    '''
+    df = pd.read_csv(clust_out, sep='\t')
+    bed_df = pd.read_csv(ref_bed, sep='\t', header=None)
+    bed_df = bed_df.iloc[:,0:6]
+    bed_df.columns = ['chr','start','end','acc','score','strand']
+    bed_df['acc'] = bed_df['acc'].str.split('.', n=1, expand=True)[0]
+
+    for i in df.columns:
+        a = df[i].tolist()
+        cleanedlist = [x for x in a if str(x) != 'NaN']
+        cleanedlist = [x for x in cleanedlist if str(x) != 'nan']
+        outname = str(i).split(' ')[0]
+        gene_fetcher(cleanedlist, outname+'_promoters.bed', bed_df, up_dist, down_dist, ref_fa)
+        outfile = outname + '_promoters.bed'
+        if ame:
+            try:
+                subprocess.check_call(['./ame_runner.sh', outfile, ref_motif])
+            except:
+                print('MEME Suite is not installed properly!')
+        else:
+            continue
+    print('Analysis complete!')
